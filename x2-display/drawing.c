943bd0dbadb9da68c0850ab57ed525d2066eb129
Edwin Park <esp@mecha1.com>
2013-09-14 01:19:19 -0400

    fixing rendering logic

--- x2-display/drawing.c
+++ x2-display/drawing.c
@@ -49,23 +49,20 @@ void *drawing_func() {
       ledscape_frame_t * const frame = ledscape_frame(leds, frame_num);
 
       // copy panel.frame -> frame
-      unsigned int quadrant = slice_idx / QUADRANT_WIDTH;  // range: 0-3
       for (unsigned int strip_idx = 0; strip_idx < LEDSCAPE_NUM_STRIPS; strip_idx++) {
         unsigned int row = strip_idx / 4;  // range: 0-6
+	unsigned int col = (4 - strip_idx) % 4;  // range: 0-3
+
         unsigned int y_offset = row * NUM_PIXELS_PER_STRIP;
         for (unsigned int pixel_idx = 0; pixel_idx < NUM_PIXELS_PER_STRIP; pixel_idx++) {
           // get RGB data from panel
           unsigned int y = y_offset + (row < 3 ? pixel_idx : NUM_PIXELS_PER_STRIP - 1 - pixel_idx);  // invert pixel_idx for lower hemisphere
-          unsigned int x = slice_idx;
+          unsigned int x = (slice_idx + (col * QUADRANT_WIDTH)) % NUM_SLICES;
           uint8_t r = panels[draw_idx][(((y * NUM_SLICES) + x) * PIXEL_SIZE) + 1];
           uint8_t g = panels[draw_idx][(((y * NUM_SLICES) + x) * PIXEL_SIZE) + 2];
           uint8_t b = panels[draw_idx][(((y * NUM_SLICES) + x) * PIXEL_SIZE) + 3];
 
-          // offset strip index according to quadrant
-          unsigned int row_origin = row * 4;  // baseline offset for row
-          unsigned int strip = row_origin + (((strip_idx - row_origin) + quadrant) % 4);  // index in row
-
-          ledscape_set_color(frame, strip_map[strip], pixel_idx, r, g, b);
+          ledscape_set_color(frame, strip_map[strip_idx], pixel_idx, r, g, b);
         }
       }
 
